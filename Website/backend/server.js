f376ff6a0ca1e73197e5be36768067e81cf20788
Deepanshu <98306877+deepanshu0912@users.noreply.github.com>
2026-01-08 21:19:45 +0530

    secons mail

--- Website/backend/server.js
+++ Website/backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const app = express();
 app.use(cors());
